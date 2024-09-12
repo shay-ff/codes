cd3f3337060ebd0a393e3ea32e1b20b581f35fb7
Mohd Ali <100376235+shay-ff@users.noreply.github.com>
2024-09-13 00:58:49 +0530

    Add files via upload

---
+++ sb/tmp.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+#define debug(x...)
+#define testcase(tc)
+#else
+#include "debug.h"
+#endif
+
+#define int long long int
+#define tp3(T) tuple<T,T,T>
+#define all(v) (v).begin(),(v).end()
+#define allr(v) (v).rbegin(),(v).rend()
+#define pr(x) cout << x << endl
+#define yesno(x) cout << ((x) ? "YES\n" : "NO\n")
+const int N = 200200, MOD = 1000000009, INF = 5e18;
+
+template <class T> bool ckmin(T &a, const T &b) {return b < a ? a = b, 1 : 0;}
+template <class T> bool ckmax(T &a, const T &b) {return b > a ? a = b, 1 : 0;}
+
+void dxt(){
+		
+}
+signed main(){
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	int tc = 1;
+	cin >> tc;
+	
+	for(int i = 1; i <= tc; ++i){
+		dxt();
+	}
+
+	return 0;
+}
